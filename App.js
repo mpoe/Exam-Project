66a3382fab93f7c608ca4ad2322bd3efcc1a293c
SMILODON_V2\Mpoe <morten_p_oe@hotmail.com>
2018-04-09 14:18:37 +0200

    quick test

--- App.js
+++ App.js
@@ -19,6 +19,7 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+//Test
 type Props = {};
 export default class App extends Component<Props> {
   render() {
