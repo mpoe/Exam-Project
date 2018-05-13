311bb034a3234f40c70e5d0e14baeafb5c1d6247
SMILODON_V2\Mpoe <morten_p_oe@hotmail.com>
2018-05-13 22:17:30 +0200

    Locked orientation

--- App/router/navigationStack.js
+++ App/router/navigationStack.js
@@ -13,33 +13,38 @@ export const AppNavigator = StackNavigator({
             tabBarVisible: false,
             header: null
         },
+        portraitOnlyMode: true
     },
     Home: {
         screen: Game,
         navigationOptions: {
             tabBarVisible: true,
             header: null
-        }
+        },
+        portraitOnlyMode: true
     },
     Login: {
         screen: Login,
         navigationOptions: {
             tabBarVisible: true,
             header: null
-        }
+        },
+        portraitOnlyMode: true
     },
     Register: {
         screen: Register,
         navigationOptions: {
             tabBarVisible: true,
             header: null
-        }
+        },
+        portraitOnlyMode: true
     },
     Game: {
         screen: Game,
         navigationOptions: {
             tabBarVisible: true,
             header: null
-        }
+        },
+        portraitOnlyMode: true
     }
 })
